84321dbc6bf7bf59df522f5c85f482a67f9749db
memonriyaz <riyazmemon614@gmail.com>
2026-03-28 12:31:14 +0530

    file structure fix

--- server/index.js
+++ server/index.js
@@ -1,30 +1,22 @@
-const express = require("express");
-const mongoose = require("mongoose");
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
 
 const app = express();
 
+// DB
+connectDB();
+
 // Middleware
+app.use(cors());
 app.use(express.json());
 
-// Import routes
-const userRoutes = require("./routes/userRoutes");
-
-// Use routes
-app.use("/api/users", userRoutes);
-
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-// Basic route
-app.get("/", (req, res) => {
-  res.send("API running");
-});
+// Routes
+// app.use("/api/auth", require("./src/routes/authRoutes"));
+// app.use("/api/events", require("./src/routes/sseRoutes"));
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`),
+);
